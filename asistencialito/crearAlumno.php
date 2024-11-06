d2431aed20967d67827914eca7bc63cd83822ab7
LucasCedrés <164428976+Lucassc5@users.noreply.github.com>
2024-11-05 21:29:49 -0300

    Martes

---
+++ asistencialito/crearAlumno.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Bienvenido Profesor</h2>
+    <h3> Registrar Alumnos </h3>
+
+    <form action="Guardar/guardarAlumno.php" method="post">
+
+    <input type="text" name="nombre_alumno" id="nombre_alumno" placeholder="Nombre">
+    <br>
+    <input type="text" name="apellido_alumno" id="apellido_alumno" placeholder="Apellido">
+    <br>
+    <input type="date" name="fecha_nacimiento_alumno" id="fecha_nacimiento_alumno" placeholder="Fecha de nacimiento">
+    <br>
+    <input type="number" name="dni_alumno" id="dni_alumno" placeholder="DNI">
+    <br>
+    <input type="submit" name="submit" value="Registrarse">
+    </form>
+
+</body>
+</html>
